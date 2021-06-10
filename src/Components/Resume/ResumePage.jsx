9a06c746307686184958f12c01d1071c8d63e67c
Zaid <zaid_lorea@yahoo.com.mx>
2021-06-10 14:09:04 -0500

    Added components for the main links

---
+++ src/Components/Resume/ResumePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Education from './Education'
+
+export default function ResumePage(){
+
+    return(
+        <div className="content">
+            <h1 >Resume Page</h1>
+            <Education />
+        </div>
+    )
+}
