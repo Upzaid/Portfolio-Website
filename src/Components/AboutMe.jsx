38f9cddf26d6aa8b7c117061f7bf932e8e82b81d
Zaid <zaid_lorea@yahoo.com.mx>
2021-06-13 15:09:29 -0500

    Added avatar and text for about page

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import Avatar from '../Assets/Avatar1.png'
 
 export default function AboutMe(){
     return(
-        <h1>About Me</h1>
+        <div className="content">
+            <span className='font-3 bold title'>About Me</span>
+            <div className='about'>
+                <img src={Avatar} className="avatar" alt=""/>
+                <div className='title-card'>
+                    <span className="font-2 bold">Hi, I'm Zaid!</span>
+                    <span className="bold">I am Civil Engineer turned into Developer, based in Houston, Tx.</span>
+                    <span className="bold">I work mainly with Javascript and I have dabbled in Python but picking up a new framework
+                        or language is not a problem. While I focus on the backend of
+                        projects, I am also comfortable working on the frontend.
+                    </span>
+                    <span className="bold">If you are a buisness seeking a web presence or an employer looking to
+                        hire, feel free to get in touch with me.
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
