2d283484e34bffd4472b7c8197d0f11c15705518
Zaid <zaid_lorea@yahoo.com.mx>
2021-06-15 09:24:29 -0500

    Finished landing page

--- src/Components/Landing/LandingPage.jsx
+++ src/Components/Landing/LandingPage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import TitleCard from './TitleCard'
-import AvatarFrame from './AvatarFrame'
 
 export default function LandingPage (){
     return(
-        <div className="content">
-            <AvatarFrame/>
-            <TitleCard />
+        <div className="content landing">
+            <div>
+                <span className="font-7 ">ZAID LOREA</span><br />
+                <span className="font-3 ">Fullstack Developer.</span><br />
+                <span className="font-1"><a href="about">About</a> <a href="projects">Projects</a> <a href="contact">Contact</a></span>
+            </div>
         </div>
     )
 }
